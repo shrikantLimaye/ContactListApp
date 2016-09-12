250c8d7a83fe24be5523f4b3ee1c1ceb1d46648f
shrikantLimaye <slimaye89@gmail.com>
2016-09-12 01:02:37 -0400

    Add files via upload
    WIll need to install Mongodb , nodejs on the system and express as well to run the project.

---
+++ controller.js
@@ -0,0 +1,52 @@
+var app = angular.module("app",[]);
+
+app.controller('AppCtrl',function($scope,$http){
+    
+var refresh = function(){
+    $http.get('/contactlist').success(function(response){
+        
+        console.log("I got data I asked for");
+        
+        $scope.contactlist = response;
+        $scope.contact = "";
+    });
+};
+    refresh();
+    
+$scope.addContact = function(){
+    console.log($scope.contact);
+    $http.post('/contactlist',$scope.contact).success(function(response){
+            
+        console.log(response);
+        refresh();
+    });
+};
+
+$scope.remove = function(id){
+    console.log(id);
+    $http.delete('/contactlist/' + id).success(function(response){
+        
+        refresh();
+        
+    });
+}
+
+$scope.edit = function(id){
+  console.log(id);
+    $http.get("/contactlist/" + id).success(function(response){
+       
+        $scope.contact = response;
+    });
+};
+    
+$scope.update = function(){
+    
+    console.log($scope.contact._id);
+    $http.put('/contactlist/' + $scope.contact._id,$scope.contact).success(function(Data){
+        refresh();
+    });
+};
+    
+    console.log("Hello World from the controller");
+    
+});
