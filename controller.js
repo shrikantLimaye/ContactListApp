94c95b528de87d5250c7d8b8aa27285021fd1a7a
shrikantLimaye <slimaye89@gmail.com>
2016-09-16 09:47:59 -0400

    Update controller.js

--- controller.js
+++ controller.js
@@ -1,19 +1,19 @@
 var app = angular.module("app",[]);
 
-app.controller('AppCtrl',function($scope,$http){
+app.controller('AppCtrl',function($scope,$http){ //common controller for all the methods to be performed
     
 var refresh = function(){
-    $http.get('/contactlist').success(function(response){
+    $http.get('/contactlist').success(function(response){ //to get the data from the database on to the browser
         
         console.log("I got data I asked for");
         
-        $scope.contactlist = response;
+        $scope.contactlist = response; //the data is fetched to the list created.
         $scope.contact = "";
     });
 };
     refresh();
     
-$scope.addContact = function(){
+$scope.addContact = function(){ // A click function to add the contacts to the database via server
     console.log($scope.contact);
     $http.post('/contactlist',$scope.contact).success(function(response){
             
@@ -22,7 +22,7 @@ $scope.addContact = function(){
     });
 };
 
-$scope.remove = function(id){
+$scope.remove = function(id){ //A delete function used to delete the contact who's ID will be specified on the time of click.
     console.log(id);
     $http.delete('/contactlist/' + id).success(function(response){
         
@@ -31,7 +31,7 @@ $scope.remove = function(id){
     });
 }
 
-$scope.edit = function(id){
+$scope.edit = function(id){ // A click function to get to the edit method for updating a contact in the list.
   console.log(id);
     $http.get("/contactlist/" + id).success(function(response){
        
@@ -39,10 +39,10 @@ $scope.edit = function(id){
     });
 };
     
-$scope.update = function(){
+$scope.update = function(){ // A update function which updates the contact clicked to its specific id.
     
     console.log($scope.contact._id);
-    $http.put('/contactlist/' + $scope.contact._id,$scope.contact).success(function(Data){
+    $http.put('/contactlist/' + $scope.contact._id,$scope.contact).success(function(Data){ //Using http.put we can then update the contact to the database and then view it on the Browser.
         refresh();
     });
 };
